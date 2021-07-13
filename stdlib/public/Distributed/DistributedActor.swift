312d0b2dc4b143f818699d004d8926cc079a9f99
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2021-07-13 12:51:35 +0900

    wip

--- stdlib/public/Distributed/DistributedActor.swift
+++ stdlib/public/Distributed/DistributedActor.swift
@@ -35,7 +35,7 @@ public protocol AnyActor: AnyObject {}
 /// which involves enqueuing new partial tasks to be executed at some
 /// point.
 @available(SwiftStdlib 5.5, *)
-public protocol DistributedActor: AnyActor, Codable {
+public protocol DistributedActor: AnyActor, Codable, Hashable, Identifiable {
 
     /// Creates new (local) distributed actor instance, bound to the passed transport.
     ///
@@ -57,7 +57,7 @@ public protocol DistributedActor: AnyActor, Codable {
     ///
     /// - Parameter address: the address to resolve, and produce an instance or proxy for.
     /// - Parameter transport: transport which should be used to resolve the `address`.
-    init(resolve address: ActorAddress, using transport: ActorTransport) throws
+    init(resolve identity: ActorIdentity, using transport: ActorTransport) throws
 
     /// The `ActorTransport` associated with this actor.
     /// It is immutable and equal to the transport passed in the local/resolve
@@ -65,7 +65,7 @@ public protocol DistributedActor: AnyActor, Codable {
     ///
     /// Conformance to this requirement is synthesized automatically for any
     /// `distributed actor` declaration.
-    nonisolated var actorTransport: ActorTransport { get }
+    nonisolated var actorTransport: ActorTransport { get } // TODO: rename to `transport`?
 
     /// Logical address which this distributed actor represents.
     ///
@@ -73,13 +73,27 @@ public protocol DistributedActor: AnyActor, Codable {
     ///
     /// Conformance to this requirement is synthesized automatically for any
     /// `distributed actor` declaration.
-    nonisolated var actorAddress: ActorAddress { get }
+    nonisolated var id: ActorIdentity { get } // NOTE: we cannot make this just ActorIdentity because the protocol would want to be hashable and equatable... and we can't do that heh
+}
+
+// ==== Hashable conformance ---------------------------------------------------
+
+extension DistributedActor {
+  public func hash(into hasher: inout Hasher) {
+    self.transport.hashIdentity(underlying, into: &hasher)
+  }
+
+  public static func == (lhs: DistributedActor, rhs: DistributedActor) -> Bool {
+    // TODO: not super great... we use the left's transport but either should/could be used
+    lhs.transport.compareIdentities(lhs.id, rhs.id)
+    // TODO: would we need to `&& rhs.transport.compare...` as well perhaps?
+  }
 }
 
 // ==== Codable conformance ----------------------------------------------------
 
 extension CodingUserInfoKey {
-@available(SwiftStdlib 5.5, *)
+    @available(SwiftStdlib 5.5, *)
     static let actorTransportKey = CodingUserInfoKey(rawValue: "$dist_act_trans")!
 }
 
@@ -97,98 +111,64 @@ extension DistributedActor {
         fatalError("\(#function) is not implemented yet for distributed actors'")
     }
 
-    // FIXME: distributed(nonisolated)
     nonisolated public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(self.actorAddress)
+//        var container = encoder.singleValueContainer()
+//        try container.encode(self.actorAddress)
+//  /Users/ktoso/code/swift-project/swift/stdlib/public/Distributed/DistributedActor.swift:110:13: error: protocol 'ActorIdentity' as a type cannot conform to 'Encodable'
+//  try container.encode(self.actorAddress)
+//      ^
+//      /Users/ktoso/code/swift-project/swift/stdlib/public/Distributed/DistributedActor.swift:110:13: note: only concrete types such as structs, enums and classes can conform to protocols
+//  try container.encode(self.actorAddress)
+//      ^
+//      /Users/ktoso/code/swift-project/swift/stdlib/public/Distributed/DistributedActor.swift:110:13: note: required by instance method 'encode' where 'T' = 'ActorIdentity'
+//  try container.encode(self.actorAddress)
+//      ^
+
+      try self.actorTransport.encodeIdentity(self.id, to: encoder)
     }
+}
+
+// ==== Equatable & Hashable conformance --------------------------------------
+
+@available(SwiftStdlib 5.5, *)
+extension DistributedActor {
+  func hash(into hasher: inout Hasher) {
+    self.id.hash(into: &hasher)
+  }
 }
 
 /******************************************************************************/
-/***************************** Actor Address **********************************/
+/***************************** Actor Identity *********************************/
 /******************************************************************************/
 
-/// Uniquely identifies a distributed actor, and enables sending messages even to remote actors.
-///
-/// ## Identity
-/// The address is the source of truth with regards to referring to a _specific_ actor in the system.
-/// This is in contrast to an `ActorPath` which can be thought of as paths in a filesystem, however without any uniqueness
-/// or identity guarantees about the files those paths point to.
-///
-/// ## Lifecycle
-/// Note, that an ActorAddress is a pure value, and as such does not "participate" in an actors lifecycle;
-/// Thus, it may represent an address of an actor that has already terminated, so attempts to locate (resolve)
-/// an `ActorRef` for this address may result with a reference to dead letters (meaning, that the actor this address
-/// had pointed to does not exist, and most likely is dead / terminated).
-///
-/// ## Serialization
-///
-/// An address can be serialized using `Codable` or other serialization mechanisms.
-/// When shared over the network or with other processes it must include the origin's
-/// system address (e.g. the network address of the host, or process identifier).
-///
-/// When using `Codable` serialization this is done automatically, by looking up
-/// the address of the `ActorTransport` the actor is associated with if was a local
-/// instance, or simply carrying the full address if it already was a remote reference.
-///
-/// ## Format
-/// The address consists of the following parts:
-///
-/// ```
-/// |              node                  | path               | incarnation |
-///  (  protocol | name? | host | port  ) ( [segments] name )? (  uint32   )
-/// ```
-///
-/// For example: `sact://human-readable-name@127.0.0.1:7337/user/wallet/id-121242`.
-/// Note that the `ActorIncarnation` is not printed by default in the String representation of a path, yet may be inspected on demand.
+/// Uniquely identifies a distributed actor, and enables sending messages and identifying remote actors.
 @available(SwiftStdlib 5.5, *)
-public struct ActorAddress: Codable, Sendable, Equatable, Hashable {
-    /// Uniquely specifies the actor transport and the protocol used by it.
-    ///
-    /// E.g. "xpc", "specific-clustering-protocol" etc.
-    public var `protocol`: String
-
-    public var host: String?
-    public var port: Int?
-    public var nodeID: UInt64?
-    public var path: String?
-
-    /// Unique Identifier of this actor.
-    public var uid: UInt64 // TODO: should we remove this
-
-    // FIXME: remove this or implement for real; this is just a hack implementation for now
-    public init(parse: String) {
-        self.protocol = "sact"
-        self.host = "xxx"
-        self.port = 7337
-        self.nodeID = 11
-        self.path = "example"
-        self.uid = 123123
-    }
+public protocol ActorIdentity:
+//    Codable,
+    Sendable
+    // , Equatable, Hashable
+{
 }
 
-// TODO: naive impl, bring in a real one
-@available(SwiftStdlib 5.5, *)
-extension ActorAddress: CustomStringConvertible {
-    public var description: String {
-        var result = `protocol`
-        result += "://"
-        if let host = host {
-            result += host
-        }
-        if let port = port {
-            result += ":\(port)"
-        }
-        // TODO: decide if we'd want to print the nodeID too here.
-        if let path = path {
-            result += "/\(path)"
-        }
-        if uid > 0 {
-            result += "#\(uid)"
-        }
-        return result
-    }
-}
+//@available(SwiftStdlib 5.5, *)
+//public struct ActorIdentity: ActorIdentity {
+//  private let underlying: ActorIdentity
+//  private let transport: ActorTransport
+//
+//  public init(underlying identity: ActorIdentity, transport: ActorTransport) {
+//    self.underlying = identity
+//    self.transport = transport
+//  }
+//
+//  public func hash(into hasher: inout Hasher) {
+//    self.transport.hashIdentity(underlying, into: &hasher)
+//  }
+//
+//  public static func == (lhs: ActorIdentity, rhs: ActorIdentity) -> Bool {
+//    // TODO: not super great... we use the left's transport but either should/could be used
+//    lhs.transport.compareIdentities(lhs.underlying, rhs.underlying)
+//  }
+//}
 
 /******************************************************************************/
 /******************************** Misc ****************************************/
