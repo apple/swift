e1280f7978305944e2e56db707fdf4daf4996c40
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2023-04-24 12:24:17 +0200

    Don't generate isolating destructor if dealloc was explicitly isolated in ObjC

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -8425,6 +8425,10 @@ public:
   /// Retrieve the Objective-C selector for destructors.
   ObjCSelector getObjCSelector() const;
 
+  /// Retrives destructor decl from the superclass, or nil if there is no
+  /// superclass
+  DestructorDecl *getSuperDeinit() const;
+
   static bool classof(const Decl *D) {
     return D->getKind() == DeclKind::Destructor;
   }
--- lib/SILGen/SILGenFunction.h
+++ lib/SILGen/SILGenFunction.h
@@ -792,6 +792,9 @@ public:
   /// and calls isolated deallocating destuctor on the right executor.
   void emitIsolatingDestructor(DestructorDecl *dd);
 
+  /// Checks if isolating destructor is needed.
+  static bool shouldEmitIsolatingDestructor(DestructorDecl *dd);
+
   /// Generates code for a struct constructor.
   /// This allocates the new 'self' value, emits the
   /// body code, then returns the final initialized 'self'.
