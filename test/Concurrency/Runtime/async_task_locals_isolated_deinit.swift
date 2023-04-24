4d231303543dc6791a4e7aa0a79c0a74956d7e5c
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2023-04-24 12:25:06 +0200

    Fixed emitting hopping to executor in async deinit and added emitting precondition check in the isolated deinit

--- test/Concurrency/Runtime/async_task_locals_isolated_deinit.swift
+++ test/Concurrency/Runtime/async_task_locals_isolated_deinit.swift
@@ -12,9 +12,9 @@ import Dispatch
 import StdlibUnittest
 
 @_silgen_name("swift_task_isCurrentExecutor")
-func isCurrentExecutor(_ executor: Builtin.Executor) -> Bool
+private func isCurrentExecutor(_ executor: Builtin.Executor) -> Bool
 
-func isCurrentExecutor(_ executor: UnownedSerialExecutor) -> Bool {
+private func isCurrentExecutor(_ executor: UnownedSerialExecutor) -> Bool {
     isCurrentExecutor(unsafeBitCast(executor, to: Builtin.Executor.self))
 }
 
@@ -28,14 +28,14 @@ actor ActorCopy {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
-  
+
   init(expectedNumber: Int, group: DispatchGroup) {
     self.expectedNumber = expectedNumber
     self.group = group
     self.probe = Probe(expectedNumber: expectedNumber, group: group)
     self.probe.probeExpectedExecutor = self.unownedExecutor
   }
-  
+
   isolated deinit {
     expectTrue(isCurrentExecutor(self.unownedExecutor))
     expectEqual(expectedNumber, TL.number)
@@ -47,14 +47,14 @@ actor ActorReset {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
-  
+
   init(expectedNumber: Int, group: DispatchGroup) {
     self.expectedNumber = expectedNumber
     self.group = group
     self.probe = Probe(expectedNumber: expectedNumber, group: group)
     self.probe.probeExpectedExecutor = self.unownedExecutor
   }
-  
+
   @resetTaskLocals
   isolated deinit {
     expectTrue(isCurrentExecutor(self.unownedExecutor))
@@ -67,15 +67,78 @@ actor ActorAPI {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
-  
+
   init(resetNumber: Int, setNumber: Int, group: DispatchGroup) {
     self.expectedNumber = resetNumber
     self.group = group
     self.probe = Probe(expectedNumber: setNumber, group: group)
     self.probe.probeExpectedExecutor = self.unownedExecutor
   }
-  
+
   isolated deinit {
+    withResetTaskLocalValues {
+      expectTrue(isCurrentExecutor(self.unownedExecutor))
+      expectEqual(expectedNumber, TL.number)
+      group.leave()
+    }
+  }
+}
+
+actor ActorCopyAsync {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
+
+  init(expectedNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = expectedNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: expectedNumber, group: group)
+    self.probe.probeExpectedExecutor = self.unownedExecutor
+  }
+
+  isolated deinit async {
+    await Task.yield()
+    expectTrue(isCurrentExecutor(self.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    group.leave()
+  }
+}
+
+actor ActorResetAsync {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
+
+  init(expectedNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = expectedNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: expectedNumber, group: group)
+    self.probe.probeExpectedExecutor = self.unownedExecutor
+  }
+
+  @resetTaskLocals
+  isolated deinit async {
+    await Task.yield()
+    expectTrue(isCurrentExecutor(self.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    group.leave()
+  }
+}
+
+actor ActorAPIAsync {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
+
+  init(resetNumber: Int, setNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = resetNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: setNumber, group: group)
+    self.probe.probeExpectedExecutor = self.unownedExecutor
+  }
+
+  isolated deinit async {
+    await Task.yield()
     withResetTaskLocalValues {
       expectTrue(isCurrentExecutor(self.unownedExecutor))
       expectEqual(expectedNumber, TL.number)
@@ -115,14 +178,14 @@ class ClassCopy: Probe {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
-  
+
   override init(expectedNumber: Int, group: DispatchGroup) {
     self.expectedNumber = expectedNumber
     self.group = group
     self.probe = Probe(expectedNumber: expectedNumber, group: group)
     super.init(expectedNumber: expectedNumber, group: group)
   }
-  
+
   @AnotherActor
   deinit {
     expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
@@ -135,14 +198,14 @@ class ClassReset: Probe {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
-  
+
   override init(expectedNumber: Int, group: DispatchGroup) {
     self.expectedNumber = expectedNumber
     self.group = group
     self.probe = Probe(expectedNumber: expectedNumber, group: group)
     super.init(expectedNumber: expectedNumber, group: group)
   }
-  
+
   @AnotherActor
   @resetTaskLocals
   deinit {
@@ -156,6 +219,75 @@ class ClassAPI: Probe {
   let expectedNumber: Int
   let group: DispatchGroup
   let probe: Probe
+
+  init(resetNumber: Int, setNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = resetNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: setNumber, group: group)
+    super.init(expectedNumber: setNumber, group: group)
+  }
+
+  @AnotherActor
+  deinit {
+    withResetTaskLocalValues {
+      expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+      expectEqual(expectedNumber, TL.number)
+      group.leave()
+    }
+  }
+}
+
+class ClassCopyAsync: Probe {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
+
+  override init(expectedNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = expectedNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: expectedNumber, group: group)
+    super.init(expectedNumber: expectedNumber, group: group)
+  }
+
+  @AnotherActor
+  deinit async {
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    await Task.yield()
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    group.leave()
+  }
+}
+
+class ClassResetAsync: Probe {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
+
+  override init(expectedNumber: Int, group: DispatchGroup) {
+    self.expectedNumber = expectedNumber
+    self.group = group
+    self.probe = Probe(expectedNumber: expectedNumber, group: group)
+    super.init(expectedNumber: expectedNumber, group: group)
+  }
+
+  @AnotherActor
+  @resetTaskLocals
+  deinit async {
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    await Task.yield()
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    expectEqual(expectedNumber, TL.number)
+    group.leave()
+  }
+}
+
+class ClassAPIAsync: Probe {
+  let expectedNumber: Int
+  let group: DispatchGroup
+  let probe: Probe
   
   init(resetNumber: Int, setNumber: Int, group: DispatchGroup) {
     self.expectedNumber = resetNumber
@@ -165,8 +297,14 @@ class ClassAPI: Probe {
   }
   
   @AnotherActor
-  deinit {
-    withResetTaskLocalValues {
+  deinit async {
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    await Task.yield()
+    expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+    await withResetTaskLocalValues {
+      expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
+      expectEqual(expectedNumber, TL.number)
+      await Task.yield()
       expectTrue(isCurrentExecutor(AnotherActor.shared.unownedExecutor))
       expectEqual(expectedNumber, TL.number)
       group.leave()
@@ -178,7 +316,47 @@ class ClassAPI: Probe {
 let tests = TestSuite("Isolated Deinit")
 
 if #available(SwiftStdlib 5.1, *) {
-  tests.test("fast path") {
+//  tests.test("sync fast path") {
+//    let group = DispatchGroup()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    Task {
+//      await TL.$number.withValue(42) {
+//        await AnotherActor.shared.performTesting {
+//          _ = ClassCopy(expectedNumber: 42, group: group)
+//          _ = ClassReset(expectedNumber: 0, group: group)
+//          _ = ClassAPI(resetNumber: 0, setNumber: 42, group: group)
+//        }
+//      }
+//    }
+//    group.wait()
+//  }
+  
+//  tests.test("sync slow path") {
+//    let group = DispatchGroup()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    Task {
+//      TL.$number.withValue(37) {
+//        _ = ActorCopy(expectedNumber: 37, group: group)
+//        _ = ActorReset(expectedNumber: 0, group: group)
+//        _ = ActorAPI(resetNumber: 0, setNumber: 37, group: group)
+//      }
+//      TL.$number.withValue(99) {
+//        _ = ClassCopy(expectedNumber: 99, group: group)
+//        _ = ClassReset(expectedNumber: 0, group: group)
+//        _ = ClassAPI(resetNumber: 0, setNumber: 99, group: group)
+//      }
+//    }
+//    group.wait()
+//  }
+  
+  tests.test("async same actor") {
     let group = DispatchGroup()
     group.enter()
     group.enter()
@@ -186,37 +364,37 @@ if #available(SwiftStdlib 5.1, *) {
     Task {
       await TL.$number.withValue(42) {
         await AnotherActor.shared.performTesting {
-          _ = ClassCopy(expectedNumber: 42, group: group)
-          _ = ClassReset(expectedNumber: 0, group: group)
-          _ = ClassAPI(resetNumber: 0, setNumber: 42, group: group)
+          _ = ClassCopyAsync(expectedNumber: 42, group: group)
+          _ = ClassResetAsync(expectedNumber: 0, group: group)
+          _ = ClassAPIAsync(resetNumber: 0, setNumber: 42, group: group)
         }
       }
     }
     group.wait()
   }
   
-  tests.test("slow path") {
-    let group = DispatchGroup()
-    group.enter()
-    group.enter()
-    group.enter()
-    group.enter()
-    group.enter()
-    group.enter()
-    Task {
-      TL.$number.withValue(37) {
-        _ = ActorCopy(expectedNumber: 37, group: group)
-        _ = ActorReset(expectedNumber: 0, group: group)
-        _ = ActorAPI(resetNumber: 0, setNumber: 37, group: group)
-      }
-      TL.$number.withValue(99) {
-        _ = ClassCopy(expectedNumber: 99, group: group)
-        _ = ClassReset(expectedNumber: 0, group: group)
-        _ = ClassAPI(resetNumber: 0, setNumber: 99, group: group)
-      }
-    }
-    group.wait()
-  }
+//  tests.test("async different actor") {
+//    let group = DispatchGroup()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    group.enter()
+//    Task {
+//      TL.$number.withValue(37) {
+//        _ = ActorCopyAsync(expectedNumber: 37, group: group)
+//        _ = ActorResetAsync(expectedNumber: 0, group: group)
+//        _ = ActorAPIAsync(resetNumber: 0, setNumber: 37, group: group)
+//      }
+//      TL.$number.withValue(99) {
+//        _ = ClassCopyAsync(expectedNumber: 99, group: group)
+//        _ = ClassResetAsync(expectedNumber: 0, group: group)
+//        _ = ClassAPIAsync(resetNumber: 0, setNumber: 99, group: group)
+//      }
+//    }
+//    group.wait()
+//  }
 }
 
 runAllTests()
