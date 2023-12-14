5bb5530b36c277ae08da090c1f31b32bf7084707
Alejandro Alonso <alejandro_alonso@apple.com>
2023-12-14 09:13:03 -0800

    Round up sizeWithMaxSize

--- stdlib/public/core/KeyPath.swift
+++ stdlib/public/core/KeyPath.swift
@@ -3478,7 +3478,7 @@ internal struct GetKeyPathClassAndInstanceSizeFromPattern
 
   mutating func finish() {
     sizeWithMaxSize = size
-    roundUpToPointerAlignment()
+    sizeWithMaxSize = MemoryLayout<Int>._roundingUpToAlignment(sizeWithMaxSize)
     sizeWithMaxSize &+= MemoryLayout<Int>.size
   }
 }
