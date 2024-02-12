f578a38d477c526378742c81a99be594e80b3c66
Mykola (Nickolas) Pokhylets <pohilets@gmail.com>
2024-02-12 14:44:01 +0100

    Minor formatting fix in benchmark/single-source/AsyncTree.swift
    Co-authored-by: Konrad `ktoso` Malawski <konrad.malawski@project13.pl>

--- benchmark/single-source/AsyncTree.swift
+++ benchmark/single-source/AsyncTree.swift
@@ -1,4 +1,4 @@
-//===--- AsyncTree.swift -------------------------------------------===//
+//===--- AsyncTree.swift -------------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
