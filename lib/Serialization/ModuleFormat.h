ebd7dfc63abf0a32e013ccf77b333e3c5a9a15eb
Amritpan Kaur <amritpankaur@gmail.com>
2024-06-03 22:09:12 -0700

    [Serialization] Increment SWIFTMODULE_VERSION_MINOR.

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -58,7 +58,8 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 877; // extend_lifetime instruction
+const uint16_t SWIFTMODULE_VERSION_MINOR =
+    878; // Property descriptors for static properties
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
